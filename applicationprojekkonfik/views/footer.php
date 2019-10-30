41203d50f34168a585473082d54c951833462fff
penalabs <penalabs@gmail.com>
2019-10-30 20:10:05 +0700

    fauzin
    remove projek konflik

--- applicationprojekkonfik/views/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo base_url();?>">Nama Resto</a>.</strong> All rights
-    reserved.
-  </footer>
